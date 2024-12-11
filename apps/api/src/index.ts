397ac5b402a04fd840125021aa9e20b38c47cc1a
Ravshan Samandarov <ravshansbox@gmail.com>
2024-12-11 11:15:30 +0300

    set up `node-pg-migrate`

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,12 @@
-const a = 1;
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { runner as pgMigrate } from 'node-pg-migrate';
+import { DATABASE_URL, MIGRATIONS_DIR, MIGRATIONS_TABLE } from './constants';
+
+(async () => {
+  await pgMigrate({
+    databaseUrl: DATABASE_URL,
+    dir: MIGRATIONS_DIR,
+    direction: 'up',
+    migrationsTable: MIGRATIONS_TABLE,
+  });
+})().catch(console.error);
