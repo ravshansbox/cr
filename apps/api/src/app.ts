f136141f3b02512ad974781dfb450cad13eb4f68
Ravshan Samandarov <ravshansbox@gmail.com>
2025-01-06 09:59:56 +0300

    improve `createToken` route (part 1)

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { tokenRouter } from './routers';
+import { ZodError } from 'zod';
+import { createToken } from './routers/tokens/createToken';
 import { HttpError } from './exceptions';
 
 export const app = express();
@@ -10,7 +11,7 @@ app.get('/', (_request, response) => {
   response.status(200).json({ status: 'ok' });
 });
 
-app.use('/tokens', tokenRouter);
+app[createToken.method](createToken.path, createToken.handler);
 
 app.use(
   (
@@ -19,8 +20,14 @@ app.use(
     response: express.Response,
     _next: express.NextFunction,
   ) => {
+    console.error(error);
+
     if (error instanceof HttpError) {
       response.status(error.status).json({ error: error.message });
+    }
+
+    if (error instanceof ZodError) {
+      response.status(422).json({ error: error.errors });
     }
 
     response.status(500).json({ error: 'Something went wrong' });
