67368703d600726fcd021865645dace26e114923
Ravshan Samandarov <ravshansbox@gmail.com>
2025-01-21 11:43:53 +0300

    session 18: improve `Context` type

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -24,6 +24,7 @@ export default tseslint.config(
   }),
   {
     rules: {
+      '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
--- apps/ui/eslint.config.mjs
+++ apps/ui/eslint.config.mjs
@@ -18,6 +18,7 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
