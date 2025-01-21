080256337fdfdf039340282dd80b81f389f9a9ac
Ravshan Samandarov <ravshansbox@gmail.com>
2025-01-21 11:30:13 +0300

    session 16 fixups

--- apps/api/src/endpoints.ts
+++ apps/api/src/endpoints.ts
@@ -2,7 +2,7 @@ import { Endpoint } from './http';
 
 export const createTokenEndpoint = new Endpoint('post', '/tokens');
 
-export const getTokenEndpoint = new Endpoint<{ id: number }>(
+export const getTokenEndpoint = new Endpoint<{ id: string }>(
   'get',
   '/tokens/:id',
 );
