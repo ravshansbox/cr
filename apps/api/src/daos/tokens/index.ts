7bf61af80b1be856c7f72c40ac22e315eaea6b97
Ravshan Samandarov <ravshansbox@gmail.com>
2025-01-22 04:47:15 +0300

    session 25: implement `getAllTokens`

--- apps/api/src/daos/tokens/index.ts
+++ apps/api/src/daos/tokens/index.ts
@@ -1,7 +1,9 @@
 import { createToken } from './createToken.js';
 import { selectById } from './selectById.js';
+import { selectByUserId } from './selectByUserId.js';
 
 export const tokenDao = {
   createToken,
   selectById,
+  selectByUserId,
 };
--- apps/api/src/exports.ts
+++ apps/api/src/exports.ts
@@ -3,3 +3,6 @@ export { type CreateTokenFetcher } from './routers/tokens/createToken.js';
 
 export { getTokenEndpoint } from './endpoints.js';
 export { type GetTokenFetcher } from './routers/tokens/getToken.js';
+
+export { getAllTokensEndpoint } from './endpoints.js';
+export { type GetAllTokensFetcher } from './routers/tokens/getAllTokens.js';
