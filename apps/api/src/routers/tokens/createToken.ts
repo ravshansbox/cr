ea6385b00baf6f2f638fa649f30a8baf872d89fe
Ravshan Samandarov <ravshansbox@gmail.com>
2025-01-22 04:01:00 +0300

    session 23: simplify `createRoute`

--- apps/api/src/routers/tokens/createToken.ts
+++ apps/api/src/routers/tokens/createToken.ts
@@ -4,40 +4,33 @@ import { tokenDao } from '../../daos/tokens/index.js';
 import { userDao } from '../../daos/users/index.js';
 import { pool } from '../../pool.js';
 import { HttpError } from '../../exceptions/index.js';
-import {
-  createHandler,
-  createRoute,
-  Fetcher,
-  HttpResponse,
-} from '../../http.js';
+import { createRoute, Fetcher, HttpResponse } from '../../http.js';
 import { createTokenEndpoint } from '../../endpoints.js';
 import { tryCatch } from '../../tryCatch.js';
 
 export const createToken = createRoute({
   endpoint: createTokenEndpoint,
-  handler: createHandler({
-    bodySchema: zod.object({
-      username: zod.string(),
-      password: zod.string(),
-    }),
-    process: async ({ body }) => {
-      const userQuery = await tryCatch(() =>
-        userDao.selectByUsername(pool, {
-          username: body.username,
-        }),
-      );
-      if (
-        !userQuery.ok ||
-        userQuery.output.password !== crypto.hash('sha256', body.password)
-      ) {
-        throw new HttpError(401, 'Invalid username or password');
-      }
-      const token = await tokenDao.createToken(pool, {
-        userId: userQuery.output.id,
-      });
-      return new HttpResponse(201, token);
-    },
+  bodySchema: zod.object({
+    username: zod.string(),
+    password: zod.string(),
   }),
+  process: async ({ body }) => {
+    const userQuery = await tryCatch(() =>
+      userDao.selectByUsername(pool, {
+        username: body.username,
+      }),
+    );
+    if (
+      !userQuery.ok ||
+      userQuery.output.password !== crypto.hash('sha256', body.password)
+    ) {
+      throw new HttpError(401, 'Invalid username or password');
+    }
+    const token = await tokenDao.createToken(pool, {
+      userId: userQuery.output.id,
+    });
+    return new HttpResponse(201, token);
+  },
 });
 
-export type CreateTokenFetcher = Fetcher<typeof createToken.handler>;
+export type CreateTokenFetcher = Fetcher<typeof createToken>;
--- apps/api/src/routers/tokens/getToken.ts
+++ apps/api/src/routers/tokens/getToken.ts
@@ -1,21 +1,14 @@
-import {
-  createHandler,
-  createRoute,
-  Fetcher,
-  HttpResponse,
-} from '../../http.js';
+import { createRoute, Fetcher, HttpResponse } from '../../http.js';
 import { tokenDao } from '../../daos/tokens/index.js';
 import { pool } from '../../pool.js';
 import { getTokenEndpoint } from '../../endpoints.js';
 
 export const getToken = createRoute({
   endpoint: getTokenEndpoint,
-  handler: createHandler({
-    process: async ({ params }) => {
-      const token = await tokenDao.selectById(pool, { id: params.id });
-      return new HttpResponse(200, token);
-    },
-  }),
+  process: async ({ params }) => {
+    const token = await tokenDao.selectById(pool, { id: params.id });
+    return new HttpResponse(200, token);
+  },
 });
 
-export type GetTokenFetcher = Fetcher<typeof getToken.handler>;
+export type GetTokenFetcher = Fetcher<typeof getToken>;
