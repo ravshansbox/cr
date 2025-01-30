555d41909c2138fe00c9865a4b6813f1b06f717d
Ravshan Samandarov <ravshansbox@gmail.com>
2025-01-30 11:05:45 +0300

    session 30: finish `Login` functionality

--- apps/ui/src/Login.tsx
+++ apps/ui/src/Login.tsx
@@ -1,19 +1,24 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { FC } from 'react';
+import { useNavigate } from 'react-router';
 import { useForm } from 'react-hook-form';
 import { Form } from './components/Form';
 import { Label } from './components/Label';
 import { Input } from './components/Input';
 import { Button } from './components/Button';
+import { api } from './api';
 
 export const Login: FC = () => {
+  const navigate = useNavigate();
   const form = useForm({ defaultValues: { username: '', password: '' } });
 
   return (
     <Form
       className="mx-auto mt-8 max-w-md"
-      onSubmit={form.handleSubmit((values) => {
-        console.log(values);
+      onSubmit={form.handleSubmit(async (values) => {
+        const token = await api.createToken({ body: values });
+        localStorage.setItem('token', token.token);
+        await navigate('/');
       })}
     >
       <Label text="Username">
