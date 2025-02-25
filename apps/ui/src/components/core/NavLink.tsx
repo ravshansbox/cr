bf769cb950ee2240fbe2da98509ce42bf54a2387
Ravshan Samandarov <ravshansbox@gmail.com>
2025-02-25 16:12:53 +0300

    [FE] improve `NavLink`

--- apps/ui/src/components/core/NavLink.tsx
+++ apps/ui/src/components/core/NavLink.tsx
@@ -1,6 +1,15 @@
-import { classed } from '@tw-classed/react';
-import { NavLink as NavLinkCore } from 'react-router';
+import { FC } from 'react';
+import { NavLink as NavLinkCore, NavLinkProps } from 'react-router';
 
-export const NavLink = classed(NavLinkCore, {
-  base: 'block rounded border border-transparent px-2 py-1 text-blue-500 transition hover:bg-blue-50 hover:text-blue-700',
-});
+export const NavLink: FC<NavLinkProps> = ({ children, ...props }) => {
+  return (
+    <NavLinkCore
+      {...props}
+      className={({ isActive }) => {
+        return `block rounded border border-transparent px-2 py-1 text-blue-500 transition hover:text-blue-700 ${isActive ? 'bg-blue-50 hover:bg-blue-100' : 'hover:bg-blue-50'}`;
+      }}
+    >
+      {children}
+    </NavLinkCore>
+  );
+};
