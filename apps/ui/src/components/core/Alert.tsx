dc671e2bc5650abbe2460234ddc41beed8d12430
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-05 18:31:42 +0300

    [FE] implement `Alert` component

---
+++ apps/ui/src/components/core/Alert.tsx
@@ -0,0 +1,40 @@
+import { classed, VariantProps } from '@tw-classed/react';
+import { ComponentProps, FC, PropsWithChildren } from 'react';
+
+const Aside = classed('aside', {
+  variants: {
+    type: {
+      info: 'text-blue-500',
+      warning: 'text-yellow-500',
+      error: 'text-red-500',
+    },
+  },
+});
+
+const typeRoleMap: Record<Type, ComponentProps<'aside'>['role']> = {
+  info: 'status',
+  warning: 'alert',
+  error: 'alert',
+};
+
+const typeAriaLiveMap: Record<Type, ComponentProps<'aside'>['aria-live']> = {
+  info: 'off',
+  warning: 'polite',
+  error: 'assertive',
+};
+
+type Type = Exclude<VariantProps<typeof Aside>['type'], undefined>;
+export const Alert: FC<PropsWithChildren<{ type?: Type }>> = ({
+  children,
+  type = 'info',
+}) => {
+  return (
+    <Aside
+      type={type}
+      role={typeRoleMap[type]}
+      aria-live={typeAriaLiveMap[type]}
+    >
+      {children}
+    </Aside>
+  );
+};
--- apps/ui/src/components/core/index.ts
+++ apps/ui/src/components/core/index.ts
@@ -1,3 +1,4 @@
+export { Alert } from './Alert';
 export { Button } from './Button';
 export { Form } from './Form';
 export { Input } from './Input';
