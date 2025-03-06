d5624ffccf7f3ce6594bc2098e83852fa2027702
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-06 03:47:30 +0300

    [FE] enhance Label component to support layout variants

--- apps/ui/src/components/core/Label.tsx
+++ apps/ui/src/components/core/Label.tsx
@@ -3,27 +3,37 @@ import { VariantProps } from '@tw-classed/react';
 import { classed } from '../../twClassed';
 
 const LabelCore = classed('label', {
-  base: 'flex flex-col',
+  base: 'flex gap-1',
   variants: {
     type: {
       info: '',
       warning: 'text-yellow-500',
       error: 'text-red-500',
     },
+    layout: {
+      row: 'flex-row items-center',
+      column: 'flex-col',
+    },
+  },
+  defaultVariants: {
+    type: 'info',
+    layout: 'column',
   },
 });
 
 type LabelProps = {
   text: string;
   type?: VariantProps<typeof LabelCore>['type'];
+  layout?: VariantProps<typeof LabelCore>['layout'];
 };
 export const Label: FC<PropsWithChildren<LabelProps>> = ({
   children,
   text,
-  type = 'info',
+  type,
+  layout,
 }) => {
   return (
-    <LabelCore type={type}>
+    <LabelCore type={type} layout={layout}>
       <span>{text}</span>
       {children}
     </LabelCore>
