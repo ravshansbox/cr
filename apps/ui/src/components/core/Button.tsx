125bede0595bebaf1546d45e35f92d1201345bf0
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-06 03:44:04 +0300

    [FE] update Button component styles to include border for normal variant

--- apps/ui/src/components/core/Button.tsx
+++ apps/ui/src/components/core/Button.tsx
@@ -4,7 +4,7 @@ export const Button = classed('button', {
   base: 'cursor-pointer rounded border px-2 py-1 transition',
   variants: {
     variant: {
-      normal: 'bg-blue-500 text-white hover:bg-blue-600',
+      normal: 'border-blue-500 bg-blue-500 text-white hover:bg-blue-600',
       link: 'border-transparent bg-transparent text-blue-500 hover:bg-blue-50 hover:text-blue-600',
     },
   },
