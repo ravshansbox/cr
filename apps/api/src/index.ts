e6c543f7688413d2e5af9f6828e8a8698e9ea602
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-12 01:57:47 +0300

    refactor migration setup to use @ravshansbox/pg-migrate package

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,22 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import { runner as pgMigrate } from 'node-pg-migrate';
-import {
-  DATABASE_URL,
-  HTTP_PORT,
-  MIGRATIONS_DIR,
-  MIGRATIONS_TABLE,
-} from './constants.js';
+ 
+import { migrateUp } from '@ravshansbox/pg-migrate';
+import { HTTP_PORT } from './constants.js';
 import { seed } from './seed.js';
 import { pool } from './pool.js';
 import { app } from './app.js';
 
 (async () => {
-  await pgMigrate({
-    databaseUrl: DATABASE_URL,
-    dir: MIGRATIONS_DIR,
-    direction: 'up',
-    migrationsTable: MIGRATIONS_TABLE,
-  });
+  await migrateUp();
 
   await seed(pool);
 
