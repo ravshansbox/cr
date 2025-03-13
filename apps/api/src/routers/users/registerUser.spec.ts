2b9f462c4e13f8fb2f3f06b1c1c88a1822e1e1dc
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-13 07:21:05 +0300

    fix broken tests

--- apps/api/src/routers/users/registerUser.spec.ts
+++ apps/api/src/routers/users/registerUser.spec.ts
@@ -1,13 +1,20 @@
 describe('registerUser', () => {
   it('should return 201 with correct body', async () => {
+    const username = `user${Date.now()}`;
+    const password = `pass${Date.now()}`;
     const response = await httpClient.request('/users', {
       method: 'POST',
-      body: { username: 'user1', password: 'pass1' },
+      body: { email: `${username}@cloudretail.pro`, username, password },
     });
     expect(response).toEqual({
       status: 201,
       body: {
-        user: { id: expect.any(Number), username: 'user1', is_verified: false },
+        user: {
+          id: expect.any(Number),
+          email: `${username}@cloudretail.pro`,
+          username,
+          is_verified: false,
+        },
         registration_valid_until: expect.any(String),
       },
     });
--- apps/api/src/setup.vitest.ts
+++ apps/api/src/setup.vitest.ts
@@ -15,5 +15,7 @@ beforeAll(() => {
 afterEach(async () => {
   await pool.query('delete from user_registrations');
   await pool.query('delete from tokens');
+  await pool.query('delete from companies_users');
+  await pool.query('delete from companies');
   await pool.query('delete from users');
 });
